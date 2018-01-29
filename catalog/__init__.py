92de84837aee4142bcfacd9c7c566683f65acdf7
Jeremy Nelson <jermnelson@gmail.com>
2018-01-29 08:30:44 -0700

    Adding Google Login

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -1,4 +1,16 @@
-from flask import Flask
+__author__ = "Jeremy Nelson"
 
-app = Flask(__name__)
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.google import make_google_blueprint
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile("config.py")
+google_login_blprt = make_google_blueprint(
+    client_id=app.config.get("GOOGLE_CLIENT_ID"),
+    client_secret=app.config.GOOGLE_CLIENT_SECRET,
+    scope=["profile", "email"])
+
+app.register_blueprint(google_login_blprt, url_prefix="/login")
+
+
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 import pkg_resources
 
 from flask import jsonify, render_template, request
+from flask_dance.contrib.google import google
 from . import app
 
 @app.route("/search", methods=["GET", "POST"])
@@ -17,6 +18,16 @@ def search_data():
 @app.route("/detail")
 def detail():
     return render_template("detail.html")
+
+@app.route("/profile")
+def profile():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    print(resp.json())
+    return "Your Google {email}".format(email=resp.json()["email"])
+
 
 @app.route("/")
 def home():
